a66bdc7ae1b3068920f234c7b999c18233a0d598
rsensenig <r.a.sensenig@gmail.com>
2023-03-11 12:38:16 -0500

    create a TaskItem component and render task lists in the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
+const taskList = [
+  {
+    id: 1,
+    name: 'Rachel Sensenig',
+    item1: 'wake up',
+    item2: 'drink coffee',
+    item3: 'crush it'
+  },
+  {
+    id: 2,
+    name: 'Kit Fenrir',
+    item1: 'teach React class',
+    item2: 'show React demo',
+    item3: 'wholesome vibes'
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task List</h1>
+      <TaskItem />
     </div>
   );
+}
+
+function TaskItem() {
+  return (
+    taskList.map(task => (
+      <ul key = {task.id}>
+        <li>Name: {task.name} </li>
+        <li>Task 1: {task.item1}</li>
+        <li>Task 2: {task.item2}</li>
+        <li>Task 3: {task.item3}</li>
+      </ul>
+    ))
+  )
 }
 
 export default App;
